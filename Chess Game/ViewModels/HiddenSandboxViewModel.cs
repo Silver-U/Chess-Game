d6f17c981b48d3dfc89075201cfeb1d625c0cc93
Silver-U <66918674+Silver-U@users.noreply.github.com>
2023-01-07 12:30:51 -0500

    saving behaviors implementation

---
+++ Chess Game/ViewModels/HiddenSandboxViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Chess_Game.Models;
+using CommunityToolkit.Mvvm;
+using Chess_Game.ViewModels;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows;
+
+namespace Chess_Game.ViewModels;
+
+public partial class HiddenSandboxViewModel
+{
+
+    public HiddenSandboxViewModel()
+    {
+    }
+
+    public void SpawnPieceSelector(object sender, MouseButtonEventArgs e)
+    {
+        if(e.ChangedButton == MouseButton.Middle && e.ButtonState == MouseButtonState.Pressed)
+        {
+        }
+        throw new NotImplementedException();
+    }
+
+    [RelayCommand]
+    public void Dass()
+    {
+        MessageBox.Show("test");
+    }
+
+    //var pop = new Popup();
+    //var block = new TextBlock();
+    //block.Text = "dasdasd";
+    //    pop.Child = block;
+    //    pop.IsOpen = true;
+}
---
+++ Chess Game/Views/HiddenSandbox/hiddenSandboxView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Chess_Game.ViewModels;
+using System.Windows.Controls;
+
+using System.Windows.Input;
+
+namespace Chess_Game.Views
+{
+    /// <summary>
+    /// Interaction logic for hiddenSandoxView.xaml
+    /// </summary>
+    public partial class hiddenSandboxView : UserControl
+    {
+        public hiddenSandboxView()
+        {
+            InitializeComponent();
+            //DataContext = new HiddenSandboxViewModel();
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            
+        }
+    }
+}
